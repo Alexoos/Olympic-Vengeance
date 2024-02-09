04d5d179780b7cd130ec64c030b9240ea5d66138
RayaneKadri <kadrirayane@gmail.com>
2024-02-09 20:50:50 +0100

    renommage webconfig -> webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-  entry: './src/app.ts',
+  entry: path.resolve(appDirectory, 'src/app.ts'), //path to the main .ts file
   output: {
-    filename: 'js/bundleName.js',
+    filename: 'js/bundleName.js', //name for the js file that is created/compiled in memory
     clean: true,
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 8080, //port that we're using for local host (localhost:8080)
     static: path.resolve(appDirectory, 'public'), //tells webpack to serve from the public folder
     hot: true,
